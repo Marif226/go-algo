453dd6b6dec3232103fca3ce47f6c0dab79f322a
Marif <77922966+Marif226@users.noreply.github.com>
2023-02-25 14:57:38 +0600

    implemented Selection Sort

---
+++ sorting/selectionSort.go
@@ -0,0 +1,22 @@
+package sorting
+
+import "errors"
+
+func SelectionSort(arr []int) error {
+	if len(arr) == 0 {
+		return errors.New("array is empty")
+	}
+
+	
+	for i := 0; i < len(arr); i++ {
+		minIndex := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+
+	return nil
+}
